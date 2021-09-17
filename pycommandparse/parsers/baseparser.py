ee78fb4bb942dd158fc213a1dd4e0f6267ad56d6
thelegendbeacon <75773965+TheLegendBeacon@users.noreply.github.com>
2021-09-17 12:33:53 +0530

    Created a BaseParser.commands list for easy access to commands.

--- pycommandparse/parsers/baseparser.py
+++ pycommandparse/parsers/baseparser.py
@@ -8,8 +8,9 @@ class BaseParser:
         self._histsize = histsize
         self._histfile = []
 
+        self.commands = [*commands]
 
-        self.commands: dict[str, Command] = {}
+        self.command_dict: dict[str, Command] = {}
         for command in commands:
             self.add_command(command=command)
 
@@ -22,24 +23,25 @@ class BaseParser:
 
         if len(args) == 0:
             docs = []
-            for command in set(self.commands.values()):
+            for command in set(self.command_dict.values()):
                 docs.append(f'{command.name}: {command.description}')
             return "\n".join(docs)
 
         commandName = args[0]
             
-        if commandName not in self.commands:
+        if commandName not in self.command_dict:
             raise CommandNotFound(f"\"{commandName}\" not found.")
 
-        command = self.commands[commandName]
+        command = self.command_dict[commandName]
         docs = f"Function \"{command.name}\"\n\tAliases: {command.aliases}\n\tUsage: {command.usage}\n\tDescription: {command.description}"
         return docs
 
     def add_command(self, command: Command):
+        self.commands.append(command)
         aliases = command.aliases
         name = command.name
         for x in [name, *aliases]:
-            self.commands[x] = command
+            self.command_dict[x] = command
     
     def command(self, name: str, aliases: list = [], usage: str = None, description: str = "No description.", number_of_arguments: int = None):
         def decorator(function):
@@ -77,18 +79,18 @@ class BaseParser:
 
         command = form_inputs[0]
 
-        if command not in self.commands:
+        if command not in self.command_dict:
             raise CommandNotFound(f"\"{command}\" not found.")
 
         arguments = form_inputs[1:]
 
-        if self.commands[command].number_of_args is not None:
-            if len(arguments) != self.commands[command].number_of_args:
-                print(self.commands[command].number_of_args)
+        if self.command_dict[command].number_of_args is not None:
+            if len(arguments) != self.command_dict[command].number_of_args:
+                print(self.command_dict[command].number_of_args)
                 raise ArgumentError
 
         self._add_to_histfile(inp)
-        return {self.commands[command]: arguments}
+        return {self.command_dict[command]: arguments}
     
     def parse_run(self, inp: str):
         parsed = self.parse(inp)
